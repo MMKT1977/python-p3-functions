73cee6a76923dbd28517c20aa8d4fda3f826ab1a
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-03-20 23:07:41 +0300

    creating functions in python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    my_greeting = "Hello, programmer!"
+    print(my_greeting)
+    return my_greeting
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    return name
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    return name
+greet_with_default()
+
 
 def add(num1, num2):
-    pass
+    sum =(num1 + num2)
+    print(sum)
+    return sum
+add(1,2)
 
 def halve(number):
-    pass
+    result =(number / 2)
+    print(result)
+    return result
+halve(4)
+
+
